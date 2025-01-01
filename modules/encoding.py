85c7921ef1d2f9e4cd6c08978c66d50d5b40d80e
isatyamks <isatyamks@gmail.com>
2025-01-01 21:20:01 +0530

    Add face encoding functionality to process images for recognition

---
+++ modules/encoding.py
@@ -0,0 +1,10 @@
+import cv2
+import face_recognition
+
+def find_encodings(images):
+    encode_list = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encode_list.append(encode)
+    return encode_list
